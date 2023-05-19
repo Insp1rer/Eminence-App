6801cfa12bccb9e64c161f390b6802ff920427f1
Insp1rer <igorkariymk@gmail.com>
2023-05-19 05:08:30 +0300

    Amplify 2.0 upd

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { StatusBar } from "expo-status-bar";
 import RootNavigator from "./src/navigation";
 
 import { NavigationContainer } from "@react-navigation/native";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
+import config from "./src/aws-exports";
 
-export default function App() {
+Amplify.configure({ ...config, Analytics: { disabled: true } });
+
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
@@ -14,3 +19,5 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App);
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,11 +1,12 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import RestaurantDetailsScreen from "../screens/RestaurantDetailsScreen";
 import DishDetailsScreen from "../screens/DishDetailsScreen";
 import Basket from "../screens/Basket";
 import OrdersScreen from "../screens/OrdersScreen";
 import OrderDetails from "../screens/OrderDetails";
+import ProfileScreen from "../screens/ProfileScreen";
 
 import { Foundation, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
@@ -19,11 +20,14 @@ const RootNavigator = () => {
   );
 };
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const HomeTabs = () => {
   return (
-    <Tab.Navigator barStyle={{ backgroundColor: "white" }}>
+    <Tab.Navigator
+      screenOptions={{ headerShown: false }}
+      barStyle={{ backgroundColor: "white" }}
+    >
       <Tab.Screen
         name="Головна"
         component={HomeStackNavigator}
@@ -44,7 +48,7 @@ const HomeTabs = () => {
       />
       <Tab.Screen
         name="Профіль"
-        component={OrdersScreen}
+        component={ProfileScreen}
         options={{
           tabBarIcon: ({ color }) => (
             <FontAwesome5 name="user-alt" size={20} color={color} />
@@ -61,7 +65,11 @@ const HomeStackNavigator = () => {
   return (
     <HomeStack.Navigator>
       <HomeStack.Screen name="Restaurants" component={HomeScreen} />
-      <HomeStack.Screen name="Restaurant" component={RestaurantDetailsScreen} />
+      <HomeStack.Screen
+        name="Restaurant"
+        component={RestaurantDetailsScreen}
+        options={{ headerShown: false }}
+      />
       <HomeStack.Screen name="Dish" component={DishDetailsScreen} />
       <HomeStack.Screen name="Basket" component={Basket} />
     </HomeStack.Navigator>
